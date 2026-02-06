f033b54710305473781b477273a1fe8954804be6
SanGianniiii <p.sangiovanniapostolocatania@gmail.com>
2026-02-07 00:15:37 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,33 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// ⚠️ INCOLLA QUI LA STESSA CONFIGURAZIONE CHE HAI MESSO NELL'INDEX
- const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyAIB56h4OiO79B1AXMiG75C_EchLcI7S5s",
     authDomain: "cantoriapp-e18b1.firebaseapp.com",
     projectId: "cantoriapp-e18b1",
     storageBucket: "cantoriapp-e18b1.firebasestorage.app",
     messagingSenderId: "21232278125",
-    appId: "1:21232278125:web:6b82fed0650b385a717d64",
-    measurementId: "G-5CDCBR02FM"
-  };
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-// Gestisce le notifiche quando l'app è in background (chiusa)
-messaging.onBackgroundMessage((payload) => {
-  console.log('Notifica ricevuta in background:', payload);
-  
-  const title = payload.notification.title;
-  const options = {
-    body: payload.notification.body,
-    icon: '/icon-192.png',
-    badge: '/icon-192.png',
-    vibrate: [200, 100, 200]
-  };
-
-  self.registration.showNotification(title, options);
-
+    appId: "1:21232278125:web:6b82fed0650b385a717d64"
 });
 
+const messaging = firebase.messaging();
+
+// Questo serve per mostrare la notifica quando il telefono è in standby
+// NON aggiungere altre logiche di 'showNotification' qui se arrivano doppie
